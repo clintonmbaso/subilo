878f288ba0101758a009e166da687216af71ee58
clintonmbaso <140145576+clintonmbaso@users.noreply.github.com>
2025-08-10 18:07:02 +0200

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -199,7 +199,7 @@ const formData = [
         marShares: 0,     marLoans: 0,      marReturns: 0,       marPending: 0,
         aprShares: 0,     aprLoans: 0,      aprReturns: 0,       aprPending: 0,
         mayShares: 0,     mayLoans: 0,      mayReturns: 0,       mayPending: 0,
-        junShares: 0,     junLoans: 0,      junReturns: 0,       junPending: 0,       circle: 'Circle 7',      relation: '0',      kinPhone: '+260971864360',       kinName: '0',      kinEmail: '@gmail.com',      
+        junShares: 0,     junLoans: 0,      junReturns: 0,       junPending: 0,       circle: 'Circle 7',      relation: 'Aunt',      kinPhone: '+260971864360',       kinName: 'Jane Mubuyaeta Nyambe',      kinEmail: '@gmail.com',      
         reg: 200,         paidOut: 0,       savings: 0,          pending: 0,          payout: 0,               loan: 0,       
         janb: false,       febb: false,      marb: false,       mayb: false,        junb: false,              julb: false,               paycheck: false
     },
